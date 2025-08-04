8e43cb685497dc3722ab88ba4589a08e10a10901
paqsa <gigapaqsa69@gmail.com>
2025-08-04 15:28:17 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const posts = [
+const posts = [
     {
       id: 1,
       author: "Lorem Ipsum",
@@ -25,23 +22,96 @@ function App() {
     }
   ]
 
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+
+
+  const [postsData, setPostsData] = useState(posts)
+
+
+
+  const handleDelete = (id) => {
+    const updatedPosts = postsData.filter((item) => item.id !== id);
+  setPostsData(updatedPosts);
+  }
+
+  const handleSubmit = (event) => {
+  event.preventDefault();
+
+  setPostsData(prevPostsData =>  [
+    ...prevPostsData, 
+    {
+      title,
+      content,      
+      author
+    }
+  ]);
+}
+
+
   return (
     <>
       <div>
 
-        {posts.map((post, index) => (
-          <div className='post'>
+        {postsData.map((post, index) => (
+          <div className='post' key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
             <p>{post.author}</p>
+            <button onClick={() => handleDelete(post.id)}>Delete</button>
           </div>
         ))}
 
       </div>
 
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         
+        <div>
+          <label htmlFor="">Title</label>
+          <input type="text" 
+
+          // We receive data for every event.
+          onChange={(event) => 
+
+            // And we add to this state
+            setTitle(event.target.value)
+          }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="">Content</label>
+          <input type="text" 
+
+          // We receive data for every event.
+          onChange={(event) => 
+
+            // And we add to this state
+            setContent(event.target.value)
+          }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="">Author</label>
+          <input type="text" 
+
+          // We receive data for every event.
+          onChange={(event) => 
+
+            // And we add to this state
+            setAuthor(event.target.value)
+          }
+          />
+        </div>
+
+        <button type='submit'>Submit</button>
 
       </form>
     </>
