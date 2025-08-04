215662a85c9bca8fda9d6756e87269f515460822
paqsa <gigapaqsa69@gmail.com>
2025-08-04 14:22:16 +0400

    lorem

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const posts = [
+    {
+      id: 1,
+      author: "Lorem Ipsum",
+      title: "What is Lorem Ipsum?",
+      content: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"
+    }
+  ]
+
+  return (
+    <>
+      <div>
+        <h1>ass</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
